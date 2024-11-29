9d90c24faa70c168284c15c357232a411eafbbb4
Avazbek07744 <javazbek87@gmail.com>
2024-11-29 09:31:27 +0500

    vazifam

---
+++ src/components/Error.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const Error: FC = () => {
+    return (
+        <div>
+            <h1>Kechirasiz bu saytda bunday page yo'q</h1>
+        </div>
+    )
+}
+
+export default Error
